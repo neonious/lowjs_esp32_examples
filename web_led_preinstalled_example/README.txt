4ba9d4417773a5ce97a11fce6ef1b4cfddb4a64a
Thomas Rogg <thomas@neonious.com>
2018-09-20 23:31:46 +0200

    added preinstalled example
    as suggested by https://github.com/sberlin
    
    (not merging pull request as the example in the pull request was old)

---
+++ web_led_preinstalled_example/README.txt
@@ -0,0 +1,17 @@
+Welcome to your neonious one!
+
+The program entry point is /src/index.js .
+
+/www/ contains the static files your program serves to the user. The contents
+of this directory are not read by the JavaScript runtime on the board, but
+rather is read by the program (/src/index.js) and served to the user. Thus, all
+JS files in /www/ run in the browser, not on neonious one.
+
+The official documentation of the board can be found at
+
+    https://www.neonious.com/docs/
+
+For any questions, we are happy to help you! Contact us at info@neonious.com at
+any time.
+
+-- The neonious team
---
+++ web_led_preinstalled_example/src/index.js
@@ -0,0 +1,55 @@
+/*
+ * neonious one example program
+ *
+ * Written by neonious, modify as you wish.
+ */
+
+let http = require('http');
+let fs = require('fs');
+let gpio = require('gpio');
+
+// Just thrown in to show how to detect a button press
+// Note: This is for the left button, the right button is reset!
+gpio.pins[gpio.BUTTON].on('fall', () => {
+    console.log('Button pressed!')
+});
+
+function writeStatic(path, res) {
+    fs.readFile('/www/' + path, (err, data) => {
+        if (err) {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('fs.readFile error: ' + err);
+            return;
+        }
+
+        let contentType = 'text/html';
+        if (path.substr(-4) == '.png')
+            contentType = 'image/png';
+        else if (path.substr(-3) == '.js')
+            contentType = 'text/javascript';
+
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(data);
+    });
+}
+
+let ledRedState = false, ledGreenState = false;
+
+http.createServer(function (req, res) {
+    // Callbacks
+    if (req.url == '/ToggleGreen') {
+        res.end();
+        console.log("Toggling green LED");
+        ledGreenState = !ledGreenState;
+        gpio.pins[gpio.LED_GREEN].setValue(ledGreenState);
+    } else if (req.url == '/ToggleRed') {
+        res.end();
+        console.log("Toggling red LED");
+        ledRedState = !ledRedState;
+        gpio.pins[gpio.LED_RED].setValue(ledRedState);
+    } else {
+        writeStatic(req.url == '/' ? '/index.html' : req.url, res);
+    }
+}).listen(80);
+
+console.log("Web server running!");
