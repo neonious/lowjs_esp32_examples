3723670187907b301e201c58eef06e7a0075e4ca
sachagloor <49924565+sachagloor@users.noreply.github.com>
2019-05-25 15:52:45 +0200

    Update dinDebounce.ts
    - Added tsc instructions
    - Added support for 0ms debounce, i.e. no debounce
    - Added pass through reference

--- drivers/input/dinDebounce.ts
+++ drivers/input/dinDebounce.ts
@@ -2,52 +2,63 @@
 /*
  * A module to debounce digital input
  *
- * Written in TypeScript. Use tsc to convert it to js if needed.
+ * Written in TypeScript. Use tsc to convert it to js if needed:
+ *
+ * npm install -g typescript // installs typescript
+ * tsc dinDebounce.ts // generates dinDebounce.js
  *
  **** HOW TO USE IT ***
  let gpio = require('gpio');
  
  import { dinDebounce } from "./dinDebounce";
  
- // Argument: PIN Number, Debouncevalue in ms, callback function
+ // Arguments: PIN Number, Debouncevalue in ms, pass through reference, callback function
 
- let myinput=new dinDebounce(27,gpio.INPUT,200,(pinValue)=> {
-    console.log('New debounced pin state: '+pinValue);
+ let myinput=new dinDebounce(27,gpio.INPUT,200,'Switch1',(pinValue,ref)=> {
+    console.log('New debounced pin state for '+ref+': '+pinValue);
  });
  
  */
-
-export class dinDebounce {
+class dinDebounce {
     private gpio: any;
     private pin: number;
     private pinState: number;
     private db: number;
     private cb: any;
     private int: any = undefined;
+    private ref: any;
 
-    constructor(pin: number, mode: number, db: number, cb: any) {
+    constructor(pin: number, mode: number, db: number, ref:any, cb: any) {
         this.gpio = require('gpio');
 
         this.pin = pin;
         this.db = db;
         this.cb = cb;
+        this.ref = ref;
 
         this.gpio.pins[pin].setType(mode);
 
         this.gpio.pins[pin].on('rise', () => {
-            this.debounce(0);
+            if(this.db===0) {
+                this.cb(this.ref,1);
+            } else {
+                this.debounce(1);
+            }
         });
         this.gpio.pins[pin].on('fall', () => {
-            this.debounce(1);
+            if(this.db===0) {
+                this.cb(this.ref,0);
+            } else {
+                this.debounce(0);
+            }
         });
-
     }
     debounce(pinState: number): void {
         this.pinState=pinState;
         if(this.int===undefined) {
             this.int=setTimeout(()=> {
                 this.int=undefined;
-                this.cb(this.pinState);
+                this.cb(this.ref,this.pinState);
             },this.db);
         }
     }
